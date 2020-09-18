37541302861c7be5e7d79f8a7b567d414bae546b
RubyMarsden <connemara.marsden@gmail.com>
2020-09-18 15:21:38 +0800

    Testing for precision vs number of iterations

--- main.py
+++ main.py
@@ -20,12 +20,45 @@ cube_inner_pts = np.array([[20, 20, 20], [20, 20, 80], [80, 80, 80], [20, 80, 80
 inner_shape = Polyhedron(xal_inner_pts, None, 1)
 shape = Polyhedron(xal_pts, inner_shape, 10)
 
+# finding the smallest 'number of sources' with change of 0.1%
+
+def test_precision(total_Ft, total_Ft_1, total_Ft_2, total_Ft_3):
+    print(total_Ft, ", ", total_Ft_1, ", ", total_Ft_2, ", ", total_Ft_3)
+    if total_Ft_1 and total_Ft_2 and total_Ft_3 is not None:
+        print(total_Ft_2, total_Ft_1)
+        if abs((total_Ft - total_Ft_1)/ total_Ft) < 0.001 and abs((total_Ft_1 - total_Ft_2)/ total_Ft_1) < 0.001 and\
+                abs((total_Ft_2 - total_Ft_3)/total_Ft_2) < 0.001:
+            return True
+    else:
+        return False
+
+
+total_Ft_cache = {}
+total_Ft_cache[0] = None
+total_Ft_cache[1] = None
+total_Ft_cache[2] = None
+total_Ft_cache[3] = None
+number_of_sources = 10000
+sources = generate_sources(shape, number_of_sources)
+n=3
+while test_precision(total_Ft_cache[n], total_Ft_cache[n-1], total_Ft_cache[n-2], total_Ft_cache[n-3]) is False:
+    number_of_sources += 10000
+    sources = generate_sources(shape, number_of_sources)
+    total_Ft = sample_total_Ft(shape, sources, shape.max_z)
+    total_Ft_cache[n+1] = total_Ft
+    n += 1
+
+print(total_Ft_cache[n], total_Ft_cache[n-1], total_Ft_cache[n-2], total_Ft_cache[n-3], len(sources))
+
+"""
 # generate a random source spread
 sources = generate_sources(shape, constants.number_of_sources)
 # plotting the crystal shape with sources
 graphing.plot_poly_with_sources(shape, sources)
-
+"""
 # calculating Ft for a range of polishing depths
+"""
 for z_cut_off in np.linspace(shape.min_z, shape.max_z, 51):
     total_Ft = sample_total_Ft(shape, sources, z_cut_off)
     print(z_cut_off, total_Ft)
+"""
